2e350685d7c2a18cd36de36a6a135cb51ee4b511
Keinth <141974145+ArnejoKeinth@users.noreply.github.com>
2024-02-17 10:48:05 +0800

    Fixed the divide_binary fucntion.
    when dividing non-integer binary input, the output is incorrect, I FIXED IT.

--- Midterm Project.py
+++ Midterm Project.py
@@ -161,8 +161,11 @@ def divide_binary(dividend, divisor):
     else:
         int1 = binary_to_integer(dividend)
         int2 = binary_to_integer(divisor)
-        result = int1 // int2
-        result = integer_to_binary(result)
+        result = int1 / int2
+        if result % 1 == 0:
+            result = integer_to_binary(result)
+        else:
+            result = mixed_to_binary(result)
 
     return result
 
